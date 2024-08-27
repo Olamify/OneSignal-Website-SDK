5cd738791d80c4400635d3ed591bdb50b62556ff
Olamide <154930010+Olamify@users.noreply.github.com>
2024-08-27 06:10:27 +0100

    Update OneSignalSDKWorker.js

--- express_webpack/OneSignalSDKWorker.js
+++ express_webpack/OneSignalSDKWorker.js
@@ -1,12 +1,11 @@
 importScripts("https://localhost:4001/sdks/web/v16/Dev-OneSignalSDK.sw.js");
 // This script should be registered as a Service Worker, not included directly
-
-if (&#39;serviceWorker&#39; in navigator) {
-    navigator.serviceWorker.register(&#39;https://cdn.jsdelivr.net/gh/Olamify/OneSignal-Website-SDK@latest/express_webpack/OneSignalSDKWorker.js&#39;)
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('cdn.jsdelivr.net/gh/Olamify/OneSignal-Website-SDK/express_webpack/OneSignalSDKWorker.js')
         .then(function(registration) {
-            console.log(&#39;Service Worker registered with scope:&#39;, registration.scope);
+            console.log('Service Worker registered:', registration);
         })
         .catch(function(error) {
-            console.log(&#39;Service Worker registration failed:&#39;, error);
+            console.log('Service Worker registration failed:', error);
         });
 }
